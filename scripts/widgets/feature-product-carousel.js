53f6bfae088325bedbbc09399b745d4257ca4857
Jason Subers <jason@calltofreedom.com>
2014-10-02 19:32:04 -0500

    Feature Product Carousel

--- scripts/widgets/feature-product-carousel.js
+++ scripts/widgets/feature-product-carousel.js
@@ -3,8 +3,25 @@ define(['modules/jquery-mozu'],
 		$(document).ready(function(){
 			$('.feature-product-carousel').slick({
 				accessibility: true,
+				slidesToShow: 1,
+				swipe: true,
+				touchMove: true,
+				autoplay: false,
+				fade: true,
 				dots: true,
-				slidesToShow: 1
+				dotsClass: 'dj-fpc-carat',
+				customPaging: function(slider, i) {
+                    return '<img src=\"/resources/images/fpc-carat.png\">';
+                }
+			});
+			
+			$('.fpc_thumb').hover(
+				function(){
+					var slideIndex = $(this).index();
+					$('.feature-product-carousel').slickGoTo( parseInt(slideIndex) );
+					$(this).addClass( "active" );
+			},	function(){
+					$(this).removeClass( "active" );
 			});
 		});
 	});
