67d7912cacb66fc44aeec12ce633b2b51b084f80
Jason Subers <jason@calltofreedom.com>
2014-10-03 10:40:37 -0500

    carousel taking form

--- scripts/widgets/feature-product-carousel.js
+++ scripts/widgets/feature-product-carousel.js
@@ -3,6 +3,7 @@ define(['modules/jquery-mozu'],
 		$(document).ready(function(){
 			$('.feature-product-carousel').slick({
 				accessibility: true,
+				arrows: false,
 				slidesToShow: 1,
 				swipe: true,
 				touchMove: true,
@@ -11,17 +12,18 @@ define(['modules/jquery-mozu'],
 				dots: true,
 				dotsClass: 'dj-fpc-carat',
 				customPaging: function(slider, i) {
-                    return '<img src=\"/resources/images/fpc-carat.png\">';
-                }
+					return '<img src=\"/resources/images/fpc-carat.png\">';
+				}
 			});
 			
 			$('.fpc_thumb').hover(
 				function(){
 					var slideIndex = $(this).index();
 					$('.feature-product-carousel').slickGoTo( parseInt(slideIndex) );
+					$(this).siblings().removeClass( "active" );
 					$(this).addClass( "active" );
 			},	function(){
-					$(this).removeClass( "active" );
+					// mouseOut event - here to prevent mouseEnter and mouseOut confusion when doing directly from thumb to thumb
 			});
 		});
 	});
